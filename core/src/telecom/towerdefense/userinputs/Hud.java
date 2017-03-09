a150a7b028468bd8f457ed7376bb6e8267d6454c
FlyzZx <nicolas.faraci@kbane.com>
2017-03-09 08:44:55 +0100

    Création d'une méthode pour savoir sur quel type de case on clique

--- core/src/telecom/towerdefense/userinputs/Hud.java
+++ core/src/telecom/towerdefense/userinputs/Hud.java
@@ -1,13 +1,10 @@
 package telecom.towerdefense.userinputs;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 
-import telecom.towerdefense.gameobjects.building.ArcherTower;
-import telecom.towerdefense.gameobjects.building.Building;
 import telecom.towerdefense.gameobjects.cases.BuildingCase;
+import telecom.towerdefense.gameobjects.cases.Case;
 import telecom.towerdefense.maps.AI;
 import telecom.towerdefense.utilities.AssetLoader;
 
@@ -33,7 +30,7 @@ public class Hud implements InputProcessor {
 	@Override
 	public boolean keyDown(int keycode) {
 		//TODO A SUPPRIMER, POUR TESTS UNIQUEMENT
-		this.aI.getCurrentMap().getListPlayerBuilding().clear();
+		//this.aI.getCurrentMap().getListPlayerBuilding().clear();
 		return true;
 	}
 
@@ -48,6 +45,19 @@ public class Hud implements InputProcessor {
 		
 		return false;
 	}
+	
+	/**
+	 * 
+	 * @param screenX
+	 * @param screenY
+	 * @return la classe de l'objet sur lequel on a cliqué
+	 */
+	private Class<? extends Case> getTypeCaseClicked(int screenX, int screenY) {
+		int xCase = screenX / AssetLoader.TXT_SIZE;
+		int yCase = (AssetLoader.SCREEN_HEIGHT - screenY) / AssetLoader.TXT_SIZE;
+		
+		return this.aI.getCurrentMap().getMapArray()[xCase][yCase].getClass();
+	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
@@ -58,7 +68,10 @@ public class Hud implements InputProcessor {
 			this.buildMenu.setEnabled(false);
 		}
 		
-		if(this.buildMenu.isEnabled() == false && this.aI.getCurrentMap().getMapArray()[xCase][yCase].getClass() == BuildingCase.class) { //Ajout d'un batiment
+		//if(this.buildMenu.isEnabled() && )
+		//TODO Gestion du click dans le building menu (Input multiplexer)
+		
+		if(this.buildMenu.isEnabled() == false && getTypeCaseClicked(screenX, screenY) == BuildingCase.class) { //Ouverture du building Menu
 			//TODO Affichage du BuildMenu
 			this.buildMenu.setPosition(new Vector2(screenX, screenY));
 			this.buildMenu.setEnabled(true);
