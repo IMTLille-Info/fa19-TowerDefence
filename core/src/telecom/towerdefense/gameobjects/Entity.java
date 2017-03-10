e77263ffae743f6c87597832c6a9988b9712cc55
FlyzZx <nicolas.faraci@kbane.com>
2017-03-11 00:14:43 +0100

    Mise à jour de la structure de base

--- core/src/telecom/towerdefense/gameobjects/Entity.java
+++ core/src/telecom/towerdefense/gameobjects/Entity.java
@@ -3,6 +3,8 @@ package telecom.towerdefense.gameobjects;
 public abstract class Entity extends GameObject {
 	protected int lifePoint;
 	protected int rangeAttack;
+	protected int speedAttack;
+	protected int damageAttack;
 	
 	public void attack(Entity target) {
 		
--- core/src/telecom/towerdefense/gameobjects/MobileEntity.java
+++ core/src/telecom/towerdefense/gameobjects/MobileEntity.java
@@ -3,8 +3,6 @@ package telecom.towerdefense.gameobjects;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class MobileEntity extends Entity {
-	
-	public void move(Vector2 direction) {
-		
-	}
+	protected Vector2 direction;
+	protected double speed;
 }
